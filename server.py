6d4b8f5d1dd2cfdf580afa698443c37cfa051827
Pedro Almeida <pedro22@ua.pt>
2018-06-14 02:22:44 +0100

    Sistema de votes e página index

--- server.py
+++ server.py
@@ -10,7 +10,6 @@
 import os.path
 import cherrypy
 import sqlite3
-import tabelas
 import json
 
 # Porta TCP para 10005 (grupo 5)
@@ -100,9 +99,38 @@ class Root:
       print(name)
       
     @cherrypy.expose
-    def vote(self, id, user, points):
-      return "VOTE"
+    def vote(self, id,points):
+      #user = cherrypy.request.headers.get('X-Remote-User') DESCOMENTAR
+      user = "andr.alves" # ELIMINAR LINHA QUANDO O PROJETO TIVER FEITO
+
+      if int(points) == 1 or int(points) == -1: 
+        dataBase = sqlite3.connect('database.db')
+        c = dataBase.cursor()
+
+        # Averiguar se utilizador já adicionou 'points' à música
+        nao_votou = c.execute('SELECT * FROM votos WHERE sid = ? AND email = ? AND points = ?', (id, user, points)).fetchone() is None
+        # Se não:
+        if nao_votou:
+
+          # Adicionar 'points' à coluna votes da música
+          c.execute('''UPDATE songs SET votes = votes + (?)
+                  WHERE id = ?''', (points, id,))
+
+          # Averiguar se existia um voto pré-existente em sentido contrário ao que se quer fazer agora
+          not_points = int(points) * -1
+          rem_voto = c.execute('SELECT * FROM votos WHERE sid = ? AND email = ? AND points = ?', (id, user, not_points)).fetchone() is None
+          
+          # Eliminar, se existir, o voto pré-existente em sentido contrário ao que se quer fazer agora
+          c.execute('DELETE FROM votos WHERE email = ? AND sid = ? AND points = ?', (user, id, not_points))
+        
+          # Se não havia um voto contrário, adicionar o novo voto
+          if rem_voto:
+            # Guardar voto na tabela votos
+            c.execute('''INSERT INTO votos (email, sid, points) 
+              VALUES (?, ?, ?)''', (user, id, points,))
+          
+          dataBase.commit()
+          c.close()
 
 
 cherrypy.quickstart(Root(), "/", config)
-
